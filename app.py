b6c5454c7a394a8088bb47c96d92f05d7168462b
Sachin <saspsachin2345@gmai.com>
2022-10-20 19:55:52 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# Importing required libs
+from flask import Flask, render_template, request
+from model import preprocess_img, predict_result
+
+# Instantiating flask app
+app = Flask(__name__)
+
+
+# Home route
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+# Prediction route
+@app.route('/prediction', methods=['POST'])
+def predict_image_file():
+    if request.method == 'POST':
+        img = preprocess_img(request.files['file'].stream)
+        pred = predict_result(img)
+        return render_template("result.html", predictions=str(pred))
+
+
+# Driver code
+if __name__ == "__main__":
+    app.run(port=9000, debug=True)
---
+++ model.py
@@ -0,0 +1,23 @@
+# Importing required libs
+from keras.models import load_model
+from keras.utils import img_to_array
+import numpy as np
+from PIL import Image
+
+# Loading model
+model = load_model("D:/SACHIN/Models/Hand-Sign-Digit-Language/digit_language_model.h5")
+
+
+# Preparing and pre-processing the image
+def preprocess_img(img_path):
+    op_img = Image.open(img_path)
+    img_resize = op_img.resize((224, 224))
+    img2arr = img_to_array(img_resize) / 255.0
+    img_reshape = img2arr.reshape(1, 224, 224, 3)
+    return img_reshape
+
+
+# Predicting function
+def predict_result(predict):
+    pred = model.predict(predict)
+    return np.argmax(pred[0], axis=-1)
