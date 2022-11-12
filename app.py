f66b675f8b47c410113c2aa766a71996ecacd3f8
Sachin <saspsachin2345@gmai.com>
2022-11-12 17:23:10 +0530

    Added error handling

--- app.py
+++ app.py
@@ -15,10 +15,15 @@ def main():
 # Prediction route
 @app.route('/prediction', methods=['POST'])
 def predict_image_file():
-    if request.method == 'POST':
-        img = preprocess_img(request.files['file'].stream)
-        pred = predict_result(img)
-        return render_template("result.html", predictions=str(pred))
+    try:
+        if request.method == 'POST':
+            img = preprocess_img(request.files['file'].stream)
+            pred = predict_result(img)
+            return render_template("result.html", predictions=str(pred))
+
+    except:
+        error = "File cannot be processed."
+        return render_template("result.html", err=error)
 
 
 # Driver code
