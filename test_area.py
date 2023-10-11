b5c0f3c607925ed38f72fce6d991e651315e0989
Minh Nguyen <minhlenguyen7@gmail.com>
2023-10-11 10:30:04 -0400

    Create the first test for the app.py (valid image)

--- test_area.py
+++ test_area.py
@@ -1,19 +1,41 @@
+import os
+import io
 import pytest
-from src.area import calculate_area_square
+from flask import Flask
+from flask.testing import FlaskClient
+from model import preprocess_img, predict_result
+from app import app
+import PIL
+from PIL import UnidentifiedImageError, Image
+# Create a test client
 
-def test_calculate_area_square():
-    assert calculate_area_square(2) == 4
-    assert calculate_area_square(2.5) == 6.25
 
-def test_calculate_area_square_negative():
-    with pytest.raises(TypeError):
-        calculate_area_square(-2)
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
-def test_calculate_area_square_string():
-    with pytest.raises(TypeError):
-        calculate_area_square("2")
+# Test the prediction route with a valid image
 
-def test_calculate_area_square_list():
-    with pytest.raises(TypeError):
-        calculate_area_square([2])
-        
+
+def test_predict_image_file_valid(client: FlaskClient, monkeypatch):
+    def mock_preprocess_img(img_stream):
+        # Mock preprocessing function
+        return "MockImage"
+
+    def mock_predict_result(img):
+        # Mock prediction function
+        return "MockResult"
+
+    # Mock the functions from the 'model' module
+    monkeypatch.setattr('model.preprocess_img', mock_preprocess_img)
+    monkeypatch.setattr('model.predict_result', mock_predict_result)
+
+    with open('Test_image.jpg', 'rb') as image_file:
+        response = client.post(
+            '/prediction', data={'file': (image_file, 'test_image.jpg')})
+
+    assert response.status_code == 200
+    # Check if "MockResult" is present in the rendered HTML response
+    assert b"Successfull" in response.data
