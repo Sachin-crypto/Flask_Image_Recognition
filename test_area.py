864f87f55c3e18e2cab4a051f9569d6b70f62723
Minh Nguyen <minhlenguyen7@gmail.com>
2023-10-11 10:31:18 -0400

    Create 2nd test case (invalid image) for the app

--- test_area.py
+++ test_area.py
@@ -39,3 +39,36 @@ def test_predict_image_file_valid(client: FlaskClient, monkeypatch):
     assert response.status_code == 200
     # Check if "MockResult" is present in the rendered HTML response
     assert b"Successfull" in response.data
+
+# Test the prediction route with an invalid image that is not an image file
+
+
+def test_predict_image_file_invalid_file(client: FlaskClient, monkeypatch):
+    def mock_preprocess_img(img_stream):
+        try:
+            Image.open(img_stream)
+        except UnidentifiedImageError:
+            raise ValueError("Invalid Image")
+
+    # Mock the function from the 'model' module
+    monkeypatch.setattr('model.preprocess_img', mock_preprocess_img)
+
+    # Create an invalid file, for example, a text file
+    with open('Test.txt', 'w') as invalid_file:
+        invalid_file.write("This is not an image.")
+
+    # Open the invalid text file for reading in binary mode
+    with open('Test.txt', 'rb') as image_file:
+        try:
+            response = client.post(
+                '/prediction', data={'file': (image_file, 'Test.txt')})
+        except PIL.UnidentifiedImageError as e:
+            # Capture the UnidentifiedImageError here, if it occurs
+            error_message = str(e)
+
+    # Check if the error message is as expected
+    assert "cannot identify image file" in error_message
+
+
+if __name__ == '__main__':
+    pytest.main()
