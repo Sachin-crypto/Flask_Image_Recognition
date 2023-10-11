54ddb6cd88af67f6557e817bbe65f83dac248886
Liam Knapp <Lknapp1110@conestogac.on.ca>
2023-10-10 21:49:23 -0400

    added pylint

---
+++ lint.py
@@ -0,0 +1,22 @@
+#lint.py
+
+from __future__ import annotations
+
+import sys
+
+from pylint import lint
+
+THRESHOLD = 8
+
+run = lint.Run(["app.py"], do_exit=False)
+
+score = run.linter.stats.global_note
+
+if score < THRESHOLD:
+
+    print("Linter failed: Score < threshold value")
+
+    sys.exit(1)
+
+
+sys.exit(0)
