b84b14147fbfba015ea6088ddd8133e5ba3e9607
jdavidIP <90657602+jdavidIP@users.noreply.github.com>
2023-10-16 14:54:01 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 # Importing required libs
+import os
 from keras.models import load_model
 from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
 
+# Determine the script's directory and construct the model file path
 script_dir = os.path.dirname(os.path.abspath(__file__))
 model_filename = "digit_model.h5"
 model_path = os.path.join(script_dir, model_filename)
@@ -19,8 +21,8 @@ def preprocess_img(img_path):
     img_reshape = img2arr.reshape(1, 224, 224, 3)
     return img_reshape
 
-
 # Predicting function
 def predict_result(predict):
     pred = model.predict(predict)
     return np.argmax(pred[0], axis=-1)
+
