7fbd36bdb84f169510e845b83ed72d486bc43a0e
jdavidIP <90657602+jdavidIP@users.noreply.github.com>
2023-10-16 11:43:31 -0400

    Update model.py

--- model.py
+++ model.py
@@ -3,11 +3,8 @@ from keras.models import load_model
 from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
-import os
 
-script_dir = os.path.dirname(__file__)
-model_file = "digit_model.h5"
-model_path = os.path.join(script_dir, model_file)
+model = load_model("C:/Users/Mateo V.G/Flask_Image_Recognition/Flask_Image_Recognition/digit_model.h5")
 
 
 # Preparing and pre-processing the image
